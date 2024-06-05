28a3ca395ef2b44a39c3b04282215537f0987ec4
zuni <zuni.zkep@gmail.com>
2024-06-05 23:05:19 +1200

    Separate formatting from writing

--- application.py
+++ application.py
@@ -16,19 +16,24 @@ def cards_from_words(words, output_filename, overwrite, senses):
     """
     Create a card from the jisho.org entry on each of WORDS - this can be in English or Japanese (kanji, kana, romaji)
     """
+    # generate csv rows for each word
+    rows: list[list[str]] = []
     for w in words:
         wr = Word.request(w)
         if wr.data[0].japanese[0].word:
             click.echo(f'Found: {wr.data[0].japanese[0].word} ({wr.data[0].japanese[0].reading})')
         else:
             click.echo(f'Found: {wr.data[0].japanese[0].reading}')
-        click.echo(f'Writing...')
+        rows.append(formatting.word_formatted(wr, formatting.VALID_FIELDS, senses))
+
+    click.echo(f'Writing...')
+    for row in rows:
         try:
-            write_item_to_csv(output_filename, formatting.word_formatted(wr, formatting.VALID_FIELDS, senses), overwrite)
+            write_item_to_csv(output_filename, row, overwrite)
         except RuntimeError as e:
-            print("Failed to write: " + e.__str__())
-        click.echo('Done.')
+            print(f"Failed to write row for {words[rows.index(row)]}: {e.__str__()}")
 
         # don't overwrite every SINGLE word as we go, that would be silly
         if overwrite:
             overwrite = False
+    click.echo('Done.')
