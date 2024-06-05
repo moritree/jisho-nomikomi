301a18ab7ca9fcb92cb4a6cc5ac7643cb5f3008d
zuni <zuni.zkep@gmail.com>
2024-06-05 21:11:04 +1200

    User decides how many sense definitions to include

--- application.py
+++ application.py
@@ -10,10 +10,12 @@ import click
 @click.option('-o', '--output-filename', default='out.csv', show_default=True)
 @click.option('-ow', '--overwrite/--no-overwrite', is_flag=True, default=False,
               help="Overwrite the contents of any existing output file")
-def word_card(japanese, output_filename, overwrite):
+@click.option('-ss', '--senses', default=1, show_default=True,
+              help="Number of sense definitions to include on the card (<=0 for all)")
+def word_card(japanese, output_filename, overwrite, senses):
     w = Word.request(japanese)
     click.echo(f'Writing card row for {japanese}')
     try:
-        write_item_to_csv(output_filename, formatting.word_formatted(w, formatting.VALID_FIELDS), overwrite)
+        write_item_to_csv(output_filename, formatting.word_formatted(w, formatting.VALID_FIELDS, senses), overwrite)
     except RuntimeError as e:
         print("Failed to write: " + e.__str__())
--- formatting.py
+++ formatting.py
@@ -1,24 +1,39 @@
+from functools import reduce
+
+import click
 from jisho_api.word.request import WordRequest
 
 VALID_FIELDS = ['vocab', 'kana', 'translation', 'part_of_speech', 'jlpt_level']
+DEFINITION_SEPARATOR_STR = "; "
 
 
-def word_formatted(word: WordRequest, csv_format: list[str]) -> list[str]:
+def word_formatted(word: WordRequest, csv_format: list[str], senses: int) -> list[str]:
     it_list = []
     for field in csv_format:
-        it_list.append(get_field(word, field))
+        it_list.append(get_field(word, field, senses))
 
     return it_list
 
 
-def get_field(word: WordRequest, field: str) -> str:
+def char_separated_str(ss: list[str], separator: str) -> str:
+    return reduce(lambda x, y: x + separator + y, ss)
+
+
+def get_field(word: WordRequest, field: str, senses: int) -> str:
     match field:
         case 'vocab':
             return word.data[0].japanese[0].word
         case 'kana':
             return word.data[0].japanese[0].reading
         case 'translation':
-            return word.data[0].senses[0].english_definitions.__str__()
+            if senses == 1:
+                return char_separated_str(word.data[0].senses[0].english_definitions, DEFINITION_SEPARATOR_STR)
+            # one string with <br> separating sense definitions
+            return reduce(lambda x, y: x + "<br>" + y,
+                          # each sense mapped to the format "def; def; ..."
+                          [char_separated_str(line.english_definitions, DEFINITION_SEPARATOR_STR)
+                           # sublist of senses list according to n sought
+                           for line in (word.data[0].senses[:senses] if senses > 0 else word.data[0].senses)])
         case 'part_of_speech':
             return word.data[0].senses[0].parts_of_speech.__str__()
         case 'jlpt_level':
