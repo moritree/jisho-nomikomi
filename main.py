1b148e41bdacc32887f6dbd07349084e2c6328f9
zuni <zuni.zkep@gmail.com>
2024-06-05 18:35:06 +1200

    Remove pycharm new project generated comments

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-import jisho_api
-from jisho_api.tokenize import Tokens
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
 from jisho_api.word import Word
 
 import formatting
@@ -12,5 +6,3 @@ from output import write_csv
 if __name__ == '__main__':
     r = Word.request('人')
     write_csv("test.csv", formatting.word_formatted(r, formatting.VALID_FIELDS))
-
-    # print(r)
