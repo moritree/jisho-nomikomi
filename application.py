b7ba1785e56755e354523d63a92b169e3144c33e
zuni <zuni.zkep@gmail.com>
2024-06-05 21:50:15 +1200

    Generalise word option, input doesn't need to be Japanese

--- application.py
+++ application.py
@@ -6,19 +6,20 @@ import click
 
 
 @click.command()
-@click.option('-j', '--japanese', help="Japanese word (kanji, kana, romaji)")
+@click.option('-w', '--word',
+              help='Word to create a card from - can be in English, or Japanese (kanji, kana, romaji)', required=True)
 @click.option('-o', '--output-filename', default='out.csv', show_default=True)
 @click.option('-ow', '--overwrite/--no-overwrite', is_flag=True, default=False,
               help="Overwrite the contents of any existing output file")
 @click.option('-ss', '--senses', default=1, show_default=True,
               help="Number of sense definitions to include on the card (<=0 for all)")
-def word_card(japanese, output_filename, overwrite, senses):
-    w = Word.request(japanese)
+def word_card(word, output_filename, overwrite, senses):
+    w = Word.request(word)
     if w.data[0].japanese[0].word:
-        click.echo(f'Found {w.data[0].japanese[0].word} ({w.data[0].japanese[0].reading})')
+        click.echo(f'Found: {w.data[0].japanese[0].word} ({w.data[0].japanese[0].reading})')
     else:
-        click.echo(f'Found {w.data[0].japanese[0].reading}')
-    click.echo(f'Writing card row...')
+        click.echo(f'Found: {w.data[0].japanese[0].reading}')
+    click.echo(f'Writing...')
     try:
         write_item_to_csv(output_filename, formatting.word_formatted(w, formatting.VALID_FIELDS, senses), overwrite)
     except RuntimeError as e:
