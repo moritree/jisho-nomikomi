498901555ed505dcfd5e04171dc7dd2e14d16335
zuni <zuni.zkep@gmail.com>
2024-06-05 18:29:34 +1200

    Write single predetermined word to csv file

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import jisho_api
+from jisho_api.tokenize import Tokens
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 from jisho_api.word import Word
-from jisho_api.word.request import WordRequest
 
 import formatting
 from output import write_csv
@@ -12,4 +12,5 @@ from output import write_csv
 if __name__ == '__main__':
     r = Word.request('人')
     write_csv("test.csv", formatting.word_formatted(r, formatting.VALID_FIELDS))
+
     # print(r)
--- output.py
+++ output.py
@@ -1,15 +1,18 @@
 import csv
 
 
-def csv_header(tags: str, deck: str) -> str:  # Anki header data
+def csv_header(tags: str = None, deck: str = None) -> str:  # Anki header data
     return ''.join([
         'separator:comma',
         f'\ndeck:{deck}' if deck else '',
-        f'\ntags:{tags}' if tags else ''
+        f'\ntags:{tags}' if tags else '',
+        '\n'
     ])
 
 
 def write_csv(filename: str, rows: list[str]):
     with (open(filename, 'w', newline='')) as csvfile:
+        csvfile.write(csv_header())
+
         writer = csv.writer(csvfile, dialect='unix')
         writer.writerow(rows)
