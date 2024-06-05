7ddf3e55cab6797db1310f250dd517f5371bcbcd
zuni <zuni.zkep@gmail.com>
2024-06-05 21:17:42 +1200

    Echo more useful feedback

--- application.py
+++ application.py
@@ -14,7 +14,11 @@ import click
               help="Number of sense definitions to include on the card (<=0 for all)")
 def word_card(japanese, output_filename, overwrite, senses):
     w = Word.request(japanese)
-    click.echo(f'Writing card row for {japanese}')
+    if w.data[0].japanese[0].word:
+        click.echo(f'Found {w.data[0].japanese[0].word} ({w.data[0].japanese[0].reading})')
+    else:
+        click.echo(f'Found {w.data[0].japanese[0].reading}')
+    click.echo(f'Writing card row...')
     try:
         write_item_to_csv(output_filename, formatting.word_formatted(w, formatting.VALID_FIELDS, senses), overwrite)
     except RuntimeError as e:
