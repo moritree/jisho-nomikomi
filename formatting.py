b7aefc3527b29e784e4ab6251368f26683ea1d11
zuni <zuni.zkep@gmail.com>
2024-06-06 16:43:49 +1200

    Check valid field for formatting right away rather than at the end of match

--- formatting.py
+++ formatting.py
@@ -2,7 +2,7 @@ from functools import reduce
 from jisho_api.word.request import WordRequest
 
 CSV_DIALECT = 'unix'
-VALID_FIELDS = ['vocab', 'kana', 'translation', 'part_of_speech', 'jlpt_level']
+VALID_FIELDS = ['vocab', 'kana', 'translation', 'part_of_speech', 'jlpt_level', 'example']
 DEFINITION_SEPARATOR_STR = '; '
 TYPE_SEPARATOR_STR = ', '
 
@@ -24,6 +24,10 @@ def char_separated_str(ss: list[str], separator: str) -> str:
 
 
 def get_field(word: WordRequest, field: str, senses: int) -> str:
+    # check valid field name
+    if field not in VALID_FIELDS:
+        raise ValueError(f'Field {field} not in {VALID_FIELDS}')
+
     sense_count = word.data[0].senses.__len__()
     # sublist of senses list according to n sought
     sublist = word.data[0].senses[:min(senses, sense_count - 1)] if senses > 0 \
@@ -55,7 +59,8 @@ def get_field(word: WordRequest, field: str, senses: int) -> str:
             if word.data[0].jlpt.__len__() > 0:
                 return word.data[0].jlpt[0][-2:].upper()
         case _:
-            raise RuntimeError('Unknown field: ' + field)
+            # this field may be populated later, but cannot be filled from the WordRequest data, so empty
+            return ''
 
 
 def csv_header(tags: str = None, deck: str = None) -> str:  # Anki header data
