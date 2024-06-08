7639ef8248fcb1a8a3d6a46de03353d2f2205562
zuni <zuni.zkep@gmail.com>
2024-06-08 21:03:52 +1200

    Remove generic json functions, RIP

--- configuration.py
+++ configuration.py
@@ -12,31 +12,6 @@ TOKEN_CACHE_FILENAME = 'token_cache.csv'
 VALID_FIELDS = ['vocab', 'kana', 'translation', 'part_of_speech', 'jlpt_level', 'example']
 
 
-def update_json(items: dict, path: Path):
-    loaded_dict = load_json(path) or {}  # current settings
-    with open(path, 'w+') as file:
-        for key, value in items.items():
-            if key in loaded_dict:
-                if value is None:
-                    # delete dict item
-                    loaded_dict.pop(key)
-                else:
-                    # update with new value
-                    loaded_dict[key] = value
-            else:
-                loaded_dict.update({key: value})
-        # write to config file
-        json.dump(loaded_dict, file)
-
-
-def load_json(path: Path) -> dict | None:
-    """Returns a dictionary, or None if the file does not exist."""
-    if not os.path.isfile(path):
-        return None
-    with open(path, 'r') as file:
-        return json.load(file)
-
-
 class LibraryCache:
     def __init__(self, cards: list[WordConfig] = None):
         self.cards = cards or []
